77eb06f8ef29e4fe711ffb8d7d8af5c5e04a37bb
SW2125\dltjd <leehj050211@gmail.com>
2021-11-01 22:37:35 +0900

    상벌점 http헤더 감춤

--- db_commands/meister.php
+++ db_commands/meister.php
@@ -1,17 +1,17 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-function load($url, $data, $isPost, $strcookie){
+function load($url, $data, $header, $strcookie){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
     curl_setopt($ch, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-    if($isPost){
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
-        curl_setopt($ch, CURLOPT_POST, true);
-    }
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
+    curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 1);
+    if($header){
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+    }
     curl_setopt($ch, CURLOPT_HTTPHEADER, array($strcookie));
     $response = curl_exec($ch);
     curl_close($ch);
@@ -39,6 +39,6 @@ $data = array(
     'pageNumber' => 1,
 	'onPageCnt' => '20',
 );
-echo mb_convert_encoding(load("https://bssm.meistergo.co.kr/ss/ss_a40j.php", $data, true, $strcookie), "UTF-8","EUC-KR");
+echo mb_convert_encoding(load("https://bssm.meistergo.co.kr/ss/ss_a40j.php", $data, false, $strcookie), "UTF-8","EUC-KR");
 load("https://bssm.meistergo.co.kr/logout.php", array(), false, $strcookie);
 ?>
